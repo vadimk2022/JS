e785ea0954fbbf5e35404bee3670dec1c2f18a2b
Vadim Kolodii <vinregion@gmail.com>
2023-02-23 16:30:33 +0200

    work in task 21

---
+++ script.js
@@ -0,0 +1,10 @@
+alert(" Привет. Введи подряд два числа!");
+let numOne = prompt("Введи первое число!", [0]);
+let numTwo = prompt("Введи второе число!", [0]);
+numOne = Number(numOne);
+numTwo = Number(numTwo);
+let sum = numOne + numTwo;
+let difference = numOne - numTwo;
+let multiply = numOne * numTwo;
+let quotient = numOne / numTwo;
+alert(`Сума: ${sum}` + `\nРізниця: ${difference}` + `\nДодаток: ${multiply}` + `\nДілення: ${quotient}`);
