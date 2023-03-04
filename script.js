986e70f8f171295f4e7228efdcf80bff025d229b
Vadim Kolodii <vinregion@gmail.com>
2023-03-04 12:16:35 +0200

    work in task 24

---
+++ script.js
@@ -0,0 +1,27 @@
+alert(" Привіт. Введи підряд два любих числа!");
+
+let numOne = prompt("Введи перше число!");
+let numTwo = prompt("Введи друге число!");
+
+if (numOne !== `` && numTwo !== `` && numOne !== ` ` && numTwo !== ` `) {
+  numOne = Number(numOne);
+  numTwo = Number(numTwo);
+  let sum = numOne + numTwo;
+  let difference = numOne - numTwo;
+  let multiply = numOne * numTwo;
+  let quotient = numOne / numTwo;
+
+  if (numTwo == 0) {
+    quotient = `Ділення на нуль !`;
+  }
+  if (numOne < numTwo) {
+    let rezus;
+    rezus = confirm(`Ви впевненні що хочете продовжити операцію ?`);
+    if (rezus == true) {
+      alert(`Різниця: ${difference}`);
+    }
+  }
+  alert(`Сума: ${sum}` + `\nРізниця: ${difference}` + `\nДодаток: ${multiply}` + `\nДілення: ${quotient}`);
+} else {
+  alert("Помилка !");
+}
