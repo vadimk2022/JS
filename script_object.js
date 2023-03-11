42523f6f9d8d6cda9dced13bd4d90b50eeb6eefe
Vadim Kolodii <vinregion@gmail.com>
2023-03-11 19:18:26 +0200

    work in task 25

---
+++ script_object.js
@@ -0,0 +1,42 @@
+const person = {
+  name: `Vadim`,
+  age: 28,
+  city: `Kiev`,
+  street: `Shevchenko`,
+};
+
+function getObjectProperties(obj) {
+  for (var key in obj) {
+    return true;
+  }
+  return false;
+}
+
+if (getObjectProperties(person) === false) {
+  console.log(`У об'єкті відсутні властивості.`);
+} else {
+  console.log(`Об'єкт має властивості.`);
+}
+
+const calc = {
+    numOne: 0,
+    numTwo: 0,
+    ask: function() {
+      this.numOne = Number(prompt("Введіть перше число:"));
+      this.numTwo = Number(prompt("Введіть друге число:"));
+    },
+  
+    sum: function() {
+      return this.numOne + this.numTwo;
+    },
+  
+    mul: function() {
+      return this.numOne * this.numTwo;
+    },
+  };
+  
+  calc.ask();
+  const sum = calc.sum();
+  const mul = calc.mul();
+  
+  alert("Сума чисел: " + sum + ". Добуток чисел: " + mul + "."); 
